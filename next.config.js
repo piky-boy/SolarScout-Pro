6dd72d5046057b7c5ee3a90777d9d94b42d8e026
piky-boy <amiosif@icloud.com>
2026-04-19 10:26:00 -0400

    vercel path fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
   output: process.env.NEXT_OUTPUT_MODE,
   productionBrowserSourceMaps: false,
-  outputFileTracingRoot: path.join(__dirname, '../'),
   typescript: {
     ignoreBuildErrors: false,
   },
